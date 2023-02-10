08d6e3c6729d33cfa9488423dffb99bba58b62b1
omarshahid1 <96484075+omarshahid1@users.noreply.github.com>
2023-02-10 15:15:32 +0500

    Add files via upload

---
+++ build/relay/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var ethers = require('ethers');
+var require$$0 = require('defender-relay-client/lib/ethers');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var ethers__default = /*#__PURE__*/_interopDefaultLegacy(ethers);
+var require$$0__default = /*#__PURE__*/_interopDefaultLegacy(require$$0);
+
+/**
+ * ABI of the MinimalForwarder contract
+ */
+const ForwarderAbi$1 = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"components":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"gas","type":"uint256"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"internalType":"struct MinimalForwarder.ForwardRequest","name":"req","type":"tuple"},{"internalType":"bytes","name":"signature","type":"bytes"}],"name":"execute","outputs":[{"internalType":"bool","name":"","type":"bool"},{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"}],"name":"getNonce","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"gas","type":"uint256"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"internalType":"struct MinimalForwarder.ForwardRequest","name":"req","type":"tuple"},{"internalType":"bytes","name":"signature","type":"bytes"}],"name":"verify","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}];
+
+var forwarder = {
+  ForwarderAbi: ForwarderAbi$1
+};
+
+var MinimalForwarder="0x91b54D0Ed62F0e0bFD9630dF8072b68110b251D6";var SimpleNFT="0x16f094E887A475Cec84F0A127120fAFc46A3248e";var require$$2 = {MinimalForwarder:MinimalForwarder,SimpleNFT:SimpleNFT};
+
+const { DefenderRelaySigner, DefenderRelayProvider } = require$$0__default["default"];
+
+const { ForwarderAbi } = forwarder;
+const ForwarderAddress = require$$2.MinimalForwarder;
+
+async function relay(forwarder, request, signature, whitelist) {
+  // Decide if we want to relay this request based on a whitelist
+  const accepts = !whitelist || whitelist.includes(request.to);
+  if (!accepts) throw new Error(`Rejected request to ${request.to}`);
+
+  // Validate request on the forwarder contract
+  const valid = await forwarder.verify(request, signature);
+  if (!valid) throw new Error(`Invalid request`);
+  
+  // Send meta-tx through relayer to the forwarder contract
+  const gasLimit = (parseInt(request.gas) + 50000).toString();
+  return await forwarder.execute(request, signature, { gasLimit });
+}
+
+async function handler(event) {
+  // Parse webhook payload
+  if (!event.request || !event.request.body) throw new Error(`Missing payload`);
+  const { request, signature } = event.request.body;
+  console.log(`Relaying`, request);
+  
+  // Initialize Relayer provider and signer, and forwarder contract
+  const credentials = { ... event };
+  const provider = new DefenderRelayProvider(credentials);
+  const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
+  const forwarder = new ethers__default["default"].Contract(ForwarderAddress, ForwarderAbi, signer);
+  
+  // Relay transaction!
+  const tx = await relay(forwarder, request, signature);
+  console.log(`Sent meta-tx: ${tx.hash}`);
+  return { txHash: tx.hash };
+}
+
+var relay_1 = {
+  handler,
+  relay,
+};
+
+module.exports = relay_1;
