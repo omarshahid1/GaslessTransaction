6bf1db33d5b35dedf1ed8910dd2709ddbaef36f1
omarshahid1 <96484075+omarshahid1@users.noreply.github.com>
2023-02-23 16:42:12 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,9 +20,9 @@ module.exports = {
       url: 'http://localhost:8545'
     },
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/A1XT2JXotvIJju1MyWalb_-d15YNeRhX',
+      url: 'https://eth-goerli.g.alchemy.com/v2/A1XT2JXotvIJju1MyWalb_-d15YNuRhX',
       accounts: [
-        `0x${"875cb80ebe97e9e8086f3be15a7239a5c90bfa5d345ed070c0b496850c4b9902"}`,
+        `0x${"958ghgvjbjbe97e9e8086f3be15a7239a5c90bfa5d345ed070c0b49ghvg56542"}`,
       ],
     },
 
